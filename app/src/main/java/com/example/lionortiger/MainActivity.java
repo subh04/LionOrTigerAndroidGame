7ab9a57eb19c15706329f2532f9e31f9a5d240e1
Subhasish <subhasish469@gmail.com>
2020-05-19 12:43:29 +0530

    commit 3

--- app/src/main/java/com/example/lionortiger/MainActivity.java
+++ app/src/main/java/com/example/lionortiger/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -17,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
     Player[] playerChoices=new Player[9];
 
     int [][] winnerRowsColumns={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    private boolean gameOver=false;
 
+    private Button btnReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,38 +38,43 @@ public class MainActivity extends AppCompatActivity {
     }
    public void imageViewIsTapped(View imageView){   //THE IMAGE TAPPED IS THE imageView
        ImageView tappedImageView= (ImageView) imageView;  //ImageView is sub class of view
-       tappedImageView.setTranslationX(-2000);
-
        int tappedImageViewTag=Integer.parseInt(tappedImageView.getTag().toString());
 
-       playerChoices[tappedImageViewTag]=currentPlayer;
-
-       if(currentPlayer==Player.one){
-           tappedImageView.setImageResource(R.drawable.tiger);
-           currentPlayer=Player.two;
-       }else{
-           tappedImageView.setImageResource(R.drawable.lion);
-           currentPlayer=Player.one;
-       }
-
-       tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
-       //Toast.makeText(getApplicationContext(),tappedImageView.getTag()+"",Toast.LENGTH_SHORT).show();
-
-       for(int[] winnerColumns : winnerRowsColumns){
-           if(playerChoices[winnerColumns[0]]==playerChoices[winnerColumns[1]]
-           && playerChoices[winnerColumns[1]]==playerChoices[winnerColumns[2]]
-           &&playerChoices[winnerColumns[0]]!=Player.no){
-
-               if(currentPlayer==Player.one){
-                   Toast.makeText(getApplicationContext(),"Player 2 is the winner",
-                           Toast.LENGTH_LONG).show();
-               }else{
-                   Toast.makeText(getApplicationContext(),
-                           "Player 1 is the winner",Toast.LENGTH_LONG).show();
+       if(playerChoices[tappedImageViewTag]==Player.no && gameOver==false) {
+           tappedImageView.setTranslationX(-2000);
 
 
+           playerChoices[tappedImageViewTag] = currentPlayer;
+
+           gameOver=true;
+
+           if (currentPlayer == Player.one) {
+               tappedImageView.setImageResource(R.drawable.tiger);
+               currentPlayer = Player.two;
+           } else {
+               tappedImageView.setImageResource(R.drawable.lion);
+               currentPlayer = Player.one;
+           }
+
+           tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
+           //Toast.makeText(getApplicationContext(),tappedImageView.getTag()+"",Toast.LENGTH_SHORT).show();
+
+           for (int[] winnerColumns : winnerRowsColumns) {
+               if (playerChoices[winnerColumns[0]] == playerChoices[winnerColumns[1]]
+                       && playerChoices[winnerColumns[1]] == playerChoices[winnerColumns[2]]
+                       && playerChoices[winnerColumns[0]] != Player.no) {
+
+                   if (currentPlayer == Player.one) {
+                       Toast.makeText(getApplicationContext(), "Player 2 is the winner",
+                               Toast.LENGTH_LONG).show();
+                   } else {
+                       Toast.makeText(getApplicationContext(),
+                               "Player 1 is the winner", Toast.LENGTH_LONG).show();
+
+
+                   }
                }
-                          }
+           }
        }
 
    }
